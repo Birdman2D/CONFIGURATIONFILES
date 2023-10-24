3c7ccd5365d01ab9ab5d8e6b9233355d5470e85f
Birdman2D <46060132+Birdman2D@users.noreply.github.com>
2023-10-25 10:58:27 +1300

    Create mason-plugins.lua

---
+++ NVIM/PLUGINS/mason-plugins.lua
@@ -0,0 +1,23 @@
+return {
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "ansible-language-server",
+      "ansible-lint",
+      "bash-language-server",
+      "hadolint",
+      "json-lsp",
+      "lua-language-server",
+      "markdownlint",
+      "pyflakes",
+      "pylint",
+      "python-lsp-server",
+      "shellcheck",
+      "terraform-ls",
+      "tflint",
+      "typescript-language-server",
+      "yaml-language-server",
+      "yamllint",
+    },
+  },
+}
